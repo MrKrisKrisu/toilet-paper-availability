4dedf862fe3df15c82f8c984a62b389cb203503b
Kristian Stöckel <git@k118.de>
2020-12-25 20:45:42 +0100

    Fixed ClassName

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use App\Models\Product;
 use Illuminate\Database\Seeder;
 
-class productSeeder extends Seeder
+class ProductSeeder extends Seeder
 {
     /**
      * Run the database seeds.
