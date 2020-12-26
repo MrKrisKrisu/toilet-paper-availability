f6928ab29d0fcae104a4950a45b0505633d4d51d
Kristian Stöckel <git@k118.de>
2020-12-26 20:53:04 +0100

    Create 2020_12_26_205130_create_index_on_product_availabilities.php

---
+++ database/migrations/2020_12_26_205130_create_index_on_product_availabilities.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateIndexOnProductAvailabilities extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::table('product_availabilities', function(Blueprint $table) {
+            $table->index(['created_at', 'product_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::table('product_availabilities', function(Blueprint $table) {
+            $table->dropIndex(['created_at', 'product_id']);
+        });
+    }
+}
