e46d5d39b806182f5a81c3f893a6e057204a0721
Kristian Stöckel <git@k118.de>
2020-12-27 00:27:08 +0100

    Reduced Query-Count

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('tpa:fetch')->everyFiveMinutes();
+        $schedule->command('tpa:fetch')->everyFifteenMinutes();
     }
 
     /**
